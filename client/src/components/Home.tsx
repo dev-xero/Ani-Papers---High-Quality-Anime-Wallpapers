d775cdad6a72209c573105ca39b958353978fbbd
CodeNinja-tech <elo48858@gmail.com>
2022-10-13 23:59:48 +0100

    Updated Search Query Prop

--- client/src/components/Home.tsx
+++ client/src/components/Home.tsx
@@ -1,12 +1,17 @@
-import React from 'react'
+import React, { useState } from 'react'
 import TopBar from './TopBar'
 import SearchBar from './ui/SearchBar'
 
 const Home: React.FC = () => {
+  const [searchQuery, setSearchQuery] = useState('')
+  const updateQuery = (value: string) => {
+    setSearchQuery(value)
+    console.log(searchQuery)
+  }
   return (
     <div className="min-h-screen bg-clr-bg-black p-[16px]">
       <TopBar />
-      <SearchBar />
+      <SearchBar updateQuery={(value: string) => updateQuery(value)} />
     </div>
   )
 }
